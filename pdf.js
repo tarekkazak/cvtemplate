030c1e2ab22558be19863dbe8695e573c2d47ab1
tarek <tarek.kazak@gmail.com>
2014-09-28 01:27:17 -0400

    chaged bootstrap row for table in compentencies, updated pdf generator script

--- pdf.js
+++ pdf.js
@@ -7,7 +7,7 @@ var client = new pdf.Pdfcrowd('tarekkazak', '1c5f2f9eae30f410552576dcee2b2d6e');
 //client.convertHtml('<html>regular HTML code</html>', pdf.sendHttpResponse(response));
 //You can convert also a web page and save it to a file:
 
-    client.convertURI('http://budget-it.azurewebsites.net/cv/index.html', pdf.saveToFile("example_com.pdf"),
+    client.convertURI('http://cvtemplate.azurewebsites.net/#/tarek', pdf.saveToFile("example_com.pdf"),
         {
             width: "8.5in",
             height: "11in",
